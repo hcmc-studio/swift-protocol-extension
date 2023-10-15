e8cf4a3fc6acdcb60e7fed7d44d2f818da55fb95
Ji-Hwan KIM <nodealdev@gmail.com>
2023-10-15 16:58:04 +0900

    [Script push]

--- Package.swift
+++ Package.swift
@@ -6,18 +6,18 @@ import PackageDescription
 let package = Package(
     name: "SwiftProtocolExtension",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftProtocolExtension",
-            targets: ["SwiftProtocolExtension"]),
+            targets: ["SwiftProtocolExtension"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "SwiftProtocolExtension"),
+            name: "SwiftProtocolExtension"
+        ),
         .testTarget(
             name: "SwiftProtocolExtensionTests",
-            dependencies: ["SwiftProtocolExtension"]),
+            dependencies: ["SwiftProtocolExtension"]
+        ),
     ]
 )
