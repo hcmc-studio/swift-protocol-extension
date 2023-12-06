96df4030064d0a1f46be736b2669759713a1e49a
Ji-Hwan KIM <nodealdev@gmail.com>
2023-12-06 16:05:53 +0900

    [Script push]

--- Sources/SwiftProtocolExtension/DomainTypes.swift
+++ Sources/SwiftProtocolExtension/DomainTypes.swift
@@ -15,14 +15,24 @@ public protocol Modifiable {
     var lastModifiedAt: Date? { get }
 }
 
+@available(*, deprecated, renamed: "Concealable", message: "Use Concealable instead.")
 public protocol Deletable {
     var deletedAt: Date? { get }
+}
+
+public protocol Concealable {
+    var concealedAt: Date? { get }
 }
 
 public extension Modifiable {
     var isModified: Bool { lastModifiedAt != nil }
 }
 
+@available(*, deprecated, renamed: "Concealable", message: "Use Concealable instead.")
 public extension Deletable {
     var isDeleted: Bool { deletedAt != nil }
+}
+
+public extension Concealable {
+    var isConcealed: Bool { concealedAt != nil }
 }
