72054deb008c1990a64e248397960252c38a381f
Ji-Hwan KIM <nodealdev@gmail.com>
2024-03-05 16:04:18 +0900

    [Script push]

--- Sources/SwiftProtocolExtension/io/response/Response+Empty.swift
+++ Sources/SwiftProtocolExtension/io/response/Response+Empty.swift
@@ -19,4 +19,23 @@ public struct EmptyResponse: Response {
         self.metadata = metadata
         self.result = result
     }
+    
+    enum CodingKeys: CodingKey {
+        case type
+        case metadata
+        case result
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.type, forKey: .type)
+        try container.encode(self.metadata, forKey: .metadata)
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.type = try container.decode(ResponseType.self, forKey: .type)
+        self.metadata = try container.decode(ResponseMetadata.self, forKey: .metadata)
+        self.result = .init()
+    }
 }
--- Tests/SwiftProtocolExtensionTests/SwiftProtocolExtensionTests.swift
+++ Tests/SwiftProtocolExtensionTests/SwiftProtocolExtensionTests.swift
@@ -1,6 +1,33 @@
 import XCTest
 @testable import SwiftProtocolExtension
 
+private let jsonDateTimeFormatter = {
+    let formatter = ISO8601DateFormatter()
+    formatter.formatOptions.insert(.withFractionalSeconds)
+    
+    return formatter
+}()
+
+private let defaultDecoder = {
+    let decoder = JSONDecoder()
+    decoder.dateDecodingStrategy = .custom({ decoder in
+        let container = try decoder.singleValueContainer()
+        var string = try container.decode(String.self)
+        if !string.contains(".") {
+            string = string.replacingOccurrences(of: "Z", with: ".0Z")
+        }
+        
+        let date = jsonDateTimeFormatter.date(from: string)
+        if let date = date {
+            return date
+        } else {
+            throw NSError(domain: "test", code: 1)
+        }
+    })
+    
+    return decoder
+}()
+
 final class SwiftProtocolExtensionTests: XCTestCase {
     func testExample() throws {
         // XCTest Documentation
@@ -8,5 +35,19 @@ final class SwiftProtocolExtensionTests: XCTestCase {
 
         // Defining Test Cases and Test Methods
         // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+    
+    func testDeserializeEmptyResponse() throws {
+        let response = """
+{
+  "type": "Empty",
+  "metadata": {
+    "acceptedAt": "2024-03-05T06:45:50.168424725Z",
+    "respondedAt": "2024-03-05T06:45:50.288414181Z"
+  }
+}
+"""
+        guard let responseData = response.data(using: .utf8) else { return }
+        let empty = try defaultDecoder.decode(EmptyResponse.self, from: responseData)
     }
 }
